81758d9c0984091b0d1dadcd1a989b14d2d0314a
Gaston Pucciarelli <gaston_pucciarelli@hotmail.com>
2022-08-31 20:33:25 -0300

    Arreglos login

--- js/login.js
+++ js/login.js
@@ -1,18 +1,37 @@
-const usuario = document.getElementById("usuario");
+const usuario = document.getElementById("email");
 const contraseña = document.getElementById("contraseña");
-const boton = document.getElementById("btn");
 
-boton.addEventListener("click", function(){
-    let Errores_ = "";
-    if(usuario.value == "" || contraseña.value == ""){
-        Errores_+=`
-           <div style="color: red;">
-           Los campos no pueden estar vacios
-           </div>
-           `
-        document.getElementById('user').innerHTML = Errores_
-        document.getElementById('contraseña').innerHTML = Errores_
-    }else{
-        window.location = "home.html"
-    }
- })
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("btn").addEventListener("click", function () {
+        let vacio = "";
+        let contra = ""
+        let entrar = false
+        let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
+        if (regexEmail.test(usuario.value) == "" || contraseña.value == "") {
+            vacio += `
+                    <div style="color: red;">
+                            Los campos no pueden estar vacios
+                    </div>
+                    `
+            usuario.focus();
+            entrar = true
+        }
+        if (contraseña.value.length < 6) {
+            contra += `
+                    <div style="color: red;">
+                        La contraseña tiene que tener almenos 6 caracteres
+                    </div>
+                    `
+            contraseña.focus()
+            entrar = true
+        }
+        if (entrar) {
+            document.getElementById('user').innerHTML = vacio
+            document.getElementById('pass').innerHTML = contra
+        } else {
+            localStorage.setItem('email', usuario.value);
+            window.location = "home.html"
+        }
+    });
+
+})
